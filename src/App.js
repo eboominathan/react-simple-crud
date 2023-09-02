36cd8ec7c62d189a8b31e74b8538cb2a05372c4a
Boominathan <eboominathan@gmail.com>
2023-09-03 05:09:33 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,71 +1,27 @@
-
-import { AddItem } from './AddItem';
 import './App.css';
-import { Content } from './Content';
-import { Footer } from './Footer';
-import { Header } from './Header'; 
-import { useState } from 'react';
-import { SearchItem } from './SearchItem';
+import React, { useState } from'react';
+function App() { 
 
+  const [name, setName] = useState('');
 
-
-function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo-list')));
-
-const [newItem,setNewItem] = useState('')
-const [search,setSearch] = useState('')
-
-const addItem = (item) => {
-  const id = items.length ? items[items.length - 1].id + 1 :0;
-  const addNewItem = {id,checked:false,item};
-  const listItems = [...items, addNewItem];
-  setItems(listItems)
-  localStorage.setItem('todo-list', JSON.stringify(listItems))
-}
-
-
-const handleCheck = (id) => {
-    const listItems = items.map((item) => (item.id) === id ? { ...item, checked: !item.checked } : item)
-    setItems(listItems)
-    localStorage.setItem('todo-list', JSON.stringify(listItems))
-}
-
-const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id)
-    setItems(listItems)
-    localStorage.setItem('todo-list', JSON.stringify(listItems))
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  if(!newItem)return; 
-  addItem(newItem);
-  setNewItem('') 
+  const handleColor = () => {
+    document.body.style.color = document.body.style.color === 'black'? 'white' : 'black';
+    
   }
-
-
-
-
+  const handleBg = (e) => {
+    
+     
+  }
   return (
     <div className="App">
-      <Header title="To do list"/>       
-      <AddItem 
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <SearchItem 
-          search={search}
-          setSearch={setSearch}          
-      />
-      <Content 
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}        
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-            />   
-      <Footer 
-        length={items.length}
-      />       
+        <span className='result'>Empty Value</span>
+        <input type="text" 
+        className='item'
+        onChange={()=> handleBg}
+        placeholder='Add color name' 
+        value={name}
+        />
+        <button onClick={() => handleColor}>Toggle Text Color</button>
     </div>
   );
 }
--- src/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Footer = ({length}) => {
- 
-  return (
-    <footer>{length} List {(length === 1 ) ? 'item' : 'items'}</footer>
-  )
-}
